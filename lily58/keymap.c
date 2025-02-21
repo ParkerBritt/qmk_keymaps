526d1b55ac2ff54b76ccb833473ad142aeea310e
parker <parker.g.britt@gmail.com>
2025-02-21 23:12:12 +0000

    refactor: adjust lily58 keymap

--- lily58/keymap.c
+++ lily58/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT(
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+         KC_ENT, KC_6, KC_7, KC_8, KC_9, KC_0,               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
          KC_ESC, KC_EXLM,   KC_AT,  KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
